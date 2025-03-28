4e4753d605be3560be7367a5b268cc7bba51f664
sashagp <124697572+sashagp@users.noreply.github.com>
2025-03-28 00:29:08 -0400

    Add files via upload

---
+++ Arduino_Code_for_URT_Communication.ino
@@ -0,0 +1,10 @@
+//Arduino Code for URT communication:
+
+// this sample code provided by www.programmingboss.com
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  Serial.println("Hello Boss");
+  delay(1500);
+}
